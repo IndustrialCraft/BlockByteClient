1f23c1899cdb07ab7b5f4a6533bdc14dbb2f803c
mmm <aaa123@pobox.sk>
2023-07-06 16:21:49 +0200

    added slot texture to gen_assets

--- run_gen_assets.sh
+++ run_gen_assets.sh
@@ -4,6 +4,7 @@ mkdir gen_assets
 cp assets/icon.png gen_assets
 cp assets/font.ttf gen_assets
 cp assets/breaking*.png gen_assets
+cp assets/slot.png gen_assets
 
 unzip ../BlockByteServer/client_content.zip -d gen_assets
 cargo run --release -- ./gen_assets localhost:4321
